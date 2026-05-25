01a028d90d54d1ee987bf2ba947c4709f2f8a521
shtyktasya <stiktasa@gmail.com>
2026-05-25 17:58:03 +0300

    Update notesRoutes.js

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllNotes, getNoteById, createNote, deleteNote, updateNote } from "../controllers/notesController";
+import { getAllNotes, getNoteById, createNote, deleteNote, updateNote } from "../controllers/notesController.js";
  const router = Router();
 router.get('/', getAllNotes);
 router.get('/:noteId', getNoteById);
