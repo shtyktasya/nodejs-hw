b28ab794a37dd8676b951bb9c205e4e2d5f6dff0
shtyktasya <stiktasa@gmail.com>
2026-05-29 01:50:05 +0300

    up

--- src/server.js
+++ src/server.js
@@ -7,18 +7,25 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
+app.use(cookieParser());
 
 app.get('/test', (req, res) => {
   res.json({ message: 'Server is working!' });
 });
 
+app.use('/auth', authRoutes);
 app.use('/notes', notesRoutes);
 app.use(errors());
 app.use(notFoundHandler);
