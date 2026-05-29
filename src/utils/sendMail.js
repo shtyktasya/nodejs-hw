7eea73afb1b2a7f767067d6329d02f6720d29366
shtyktasya <stiktasa@gmail.com>
2026-05-30 02:29:01 +0300

    up

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -7,6 +7,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false
+  },
 });
 
 export const sendMail = async (options) => {
